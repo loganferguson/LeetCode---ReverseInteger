1c748ff04c0d9507c86373228d66b2a5f0c8099c
Logan Ferguson <lferg1292@yahoo.com>
2020-03-05 12:26:56 -0500

    Add project files.

---
+++ ReverseInteger/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse(4589));
+            Console.ReadLine();
+        }
+
+        static int Reverse(int x)
+        {
+            try
+            {
+                if (x < Int32.MinValue)
+                {
+                    return 0;
+                }
+                string input = Convert.ToString(x);
+
+                int index = input.Length - 1;
+                string reverse = "";
+
+
+                for (int i = index; i >= 0; i--)
+                {
+                    if (input[i] == '-')
+                    {
+                        int revNegative = int.Parse(reverse) * -1;
+                        return revNegative;
+                    }
+                    reverse += input[i];
+                }
+                int rev = int.Parse(reverse);
+
+                return rev;
+            }
+            catch (OverflowException)
+            {
+                return 0;
+            }
+        }
+    }
+}
